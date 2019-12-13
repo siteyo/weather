aac8612b1528ab750767f1114aa3024a48fcba08
siteyo <phicyanoc@gmail.com>
2019-12-14 01:39:39 +0900

    コンポーネントの素を作成

---
+++ src/components/Weather/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+import WeatherContainer from 'container/Weather/WeatherContainer';
+
+const Weather: FC = () => <WeatherContainer />;
+
+export default Weather;
