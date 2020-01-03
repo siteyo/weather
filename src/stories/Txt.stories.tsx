8a4800acc67569c45b523405e0af3c66bc4e1c9a
siteyo <phicyanoc@gmail.com>
2020-01-03 18:21:41 +0900

    Txtのstorybookを追加

---
+++ src/stories/Txt.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SmallTxt, MidiumTxt, LargeTxt } from 'components/common/atoms/Txt';
+
+export default {
+  title: 'Txt',
+};
+
+export const smallTxt = () => <SmallTxt>SmallTxt</SmallTxt>;
+export const midiumTxt = () => <MidiumTxt>MidiumTxt</MidiumTxt>;
+export const largeTxt = () => <LargeTxt>LargeTxt</LargeTxt>;
